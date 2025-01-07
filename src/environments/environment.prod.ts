d3f40ddd82076e7c3c070adf3997c84bd77e7a5a
thembalakhe <m.themba1738@gmail.com>
2025-01-07 21:01:00 +0200

    try 2 fixing env deploy bug, updated environments and gitignore. set to use env-var

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+import { Environment } from "./environment";
+
+export const environment: Environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: process.env['FIREBASE_API_KEY'] || 'V_SxUD7Lk',
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN'] || '0.firebaseapp.com',
+    projectId: process.env['FIREBASE_PROJECT_ID'] || '-870',
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] || 'age.app',
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] || '9687',
+    appId: process.env['FIREBASE_APP_ID'] || '1:968847d6',
+    measurementId: process.env['FIREBASE_MEASUREMENT_ID'] || 'GT6L',
+    databaseURL: process.env['FIREBASE_DATABASE_URL'] || '.europe-wtabase.app/',
+  },
+};
+
