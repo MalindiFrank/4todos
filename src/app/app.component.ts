3da7cb2cb109481ac28854ff6657df88e816c2ce
thembalakhe <m.themba1738@gmail.com>
2025-01-07 19:49:21 +0200

    set up routes for pages, auth guard for home page and code to initialize firebase services after auth pass check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
+import { initializeApp } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = '4todos';
+
+  //initialize firebase
+  constructor() {
+    initializeApp(environment.firebaseConfig);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { AuthGuard } from './auth/auth.guard';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component'; 
+import { SignupComponent } from './pages/signup/signup.component'; 
+import { HomeComponent } from './pages/home/home.component'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  //{ path: '', loadChildren: () => import('./app.routes.ts').then(m => m.MainModule), canActivate: [AuthGuard] },
+  //{path: '**', component: HomeComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
