7d3a365bee02d16b2f5f20e3dad44972028918c3
thembalakhe <m.themba1738@gmail.com>
2025-01-07 19:41:12 +0200

    populated code for home page and taskview comp, user can now see tasks and perform certain actions

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
+import { DatabaseService } from '../../db/db.service';
+import { CommonModule } from '@angular/common';
+import { TaskViewComponent } from '../../components/taskview/taskview.component';
+
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, TaskViewComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    public dbService: DatabaseService
+  ) {}
+
+  ngOnInit() {
+    this.authService.checkAuthState().subscribe((isAuthenticated) => {
+      if (!isAuthenticated) {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+
+  onLogout() {
+    this.authService.logout().then(() => {
+      this.router.navigate(['/login']);
+    });
+  }
 
 }
